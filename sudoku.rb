3ec5883a1bc948ae597af4e1bc0b1f0d225f6ada
Guest User <aleah.luk@gmail.com>
2016-06-10 15:30:40 +0800

    debugging

--- sudoku.rb
+++ sudoku.rb
@@ -1,8 +1,10 @@
 class Sudoku
     def initialize(board_string)
-        @horizontal_arr = board_string.to_s.split("").map { |digit| digit.to_i }
-        @board_horizontal = @horizontal_arr.each_slice(9).to_a
-        @board_squares = []
+        @backtrack_array = board_string.to_s.split("").map { |digit| digit.to_i }
+        @backtrack_board = @backtrack_array.each_slice(9).to_a
+        @board_horizontal = @backtrack_board.dup
+        @horizontal_arr = @backtrack_array.dup
+        @backtrack_squares = []
         square_arr = @board_horizontal.dup
         3.times do
             3.times do
@@ -12,10 +14,11 @@ class Sudoku
                     selected.each {|x| temp_board << x}
                     square_arr[i] = square_arr[i].drop(3)
                 end
-                @board_squares << temp_board
+                @backtrack_squares << temp_board
             end
             square_arr = square_arr.drop(3)
         end
+        @board_squares = @backtrack_squares.dup
         @counter = @horizontal_arr.count(0)
         @squares_x = [[0, 0, 0, 1, 1, 1, 2, 2, 2],
         [0, 0, 0, 1, 1, 1, 2, 2, 2],
@@ -58,11 +61,12 @@ class Sudoku
     def multiple?(board)
         count = 0
         board.each do |row|
-            if row.uniq.length == row.length
-                return true
+            if row.uniq.length == row.length && row.include?(0) == false
+                count += 1
                 else return false
             end
         end
+        return true if count == 9
     end
     
     def run_through
@@ -89,42 +93,41 @@ class Sudoku
     
     def solve!
         run_through
-        @backtrack_board = @board_horizontal.dup
-        @backtrack_squares = @board_squares.dup
-        @backtrack_array = @horizontal_arr.dup
         @guess_counter = @horizontal_arr.count(0)
         if @counter == 0
             return @board_horizontal
-            else
-            boards_check = false
-            until boards_check == true
-                @board_horizontal = @backtrack_board.dup
-                @horizontal_arr = @backtrack_array.dup
-                @guess_counter = @horizontal_arr.count(0)
-                zeros = []
-                @horizontal_arr.each_with_index {|value, place| zeros << place if value == 0}
-                zeros.each do |x|
-                    possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                    possible -= @board_horizontal[x/9]
-                    possible -= @board_horizontal.transpose[x%9]
-                    position = locate(x)
-                    possible -= position
-                    if possible.empty? == false
-                        guess = possible.sample
-                        @board_horizontal[x/9][x%9] = guess
-                        @board_squares[@squares_x[x/9][x%9]][@squares_y[x/9][x%9]] = guess
-                        possible -= [possible.sample]
-                        zeros -= [x]
-                    end
-                    10.times do
-                        run_through
-                    end
-                    if multiple?(@board_horizontal) == true && multiple?(@board_horizontal.transpose) == true && multiple?(@board_squares) == true
-                        boards_check = true
-                    end
+        end
+        boards_check = false
+        until boards_check == true
+            @board_horizontal = @backtrack_board.dup
+            @horizontal_arr = @backtrack_array.dup
+            @board_squares = @backtrack_squares.dup
+            
+            @guess_counter = @horizontal_arr.count(0)
+            p @board_horizontal
+            p @backtrack_board
+            zeros = []
+            @horizontal_arr.each_with_index {|value, place| zeros << place if value == 0}
+            zeros.each do |x|
+                possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                possible -= @board_horizontal[x/9]
+                possible -= @board_horizontal.transpose[x%9]
+                position = locate(x)
+                possible -= position
+                if possible.empty? == false
+                    guess = possible.sample
+                    @board_horizontal[x/9][x%9] = guess
+                    @board_squares[@squares_x[x/9][x%9]][@squares_y[x/9][x%9]] = guess
+                    possible -= [possible.sample]
+                    zeros -= [x]
                 end
-                @horizontal_arr = @board_horizontal.flatten
+                run_through
+                if multiple?(@board_horizontal) == true && multiple?(@board_horizontal.transpose) == true && multiple?(@board_squares) == true
+                    boards_check = true
+                end
             end
+                @horizontal_arr = @board_horizontal.flatten
+                p @board_horizontal
         end
     end
     
@@ -140,7 +143,7 @@ end
 # String#chomp to remove them.
 #board_string = File.readlines('sample.unsolved.txt').first.chomp
 
-game = Sudoku.new('000689100800000029150000008403000050200005000090240801084700910500000060060410000')
+game = Sudoku.new('300000000050703008000028070700000043000000000003904105400300800100040000968000200')
 
 # Remember: this will just fill out what it can and not "guess"
 game.solve!
